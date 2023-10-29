80875956ddffe035a80ca4d7375f37545ae77879
Utku Üstüner <utkuustuner@proton.me>
2023-10-29 16:54:03 +0300

    Add A73 props

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -9,4 +9,6 @@ elif echo "$model" | grep -q "A525"; then
         mv "$MODPATH"/system_a52q.prop "$MODPATH"/system.prop
 elif echo "$model" | grep -q "A725"; then
         mv "$MODPATH"/system_a72q.prop "$MODPATH"/system.prop
+elif echo "$model" | grep -q "A736"; then
+        mv "$MODPATH"/system_a73xq.prop "$MODPATH"/system.prop
 fi
